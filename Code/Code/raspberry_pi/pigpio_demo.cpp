fc9ed96df33d5dbba96dbed3dc5764f87a16c626
Ahmed Ansari PI <ahmedalam97@gmail.com>
2025-03-07 17:07:02 -0600

    SPI code modified

--- Code/Code/raspberry_pi/pigpio_demo.cpp
+++ Code/Code/raspberry_pi/pigpio_demo.cpp
@@ -1,56 +1,87 @@
 #include <iostream>
 #include <pigpio.h>
 #include <unistd.h>
+#include <vector>
+#include <cstdlib>
 // #include <wiringPi.h>
 // #include <wiringPiSPI.h>
 
 using namespace std;
 
-const uint spiChannel = 1;
+const uint spiChannel = 0;        // CE_0 on SPI0
 const uint spiSpeed = 500 * 1000; // hertz
+const uint spiFlags = 0;          // SPI mode
 
-const int data_len = 1;
-char spiData[data_len];
+const int data_len = 4;
+char spiTxData[data_len];
+char spiRxData[data_len];
 
-int spiReturnVal;
+// vector<char> txbuffer(data_len);
 
 int main()
 {
-    while (gpioInitialise() < 0)
+    // initialize pigpio
+    int pi = gpioInitialise();
+    if (pi < 0)
     {
-        cout << "Error initializing pigpio" << endl;
+        cerr << "Error initializing pigpio" << endl;
         usleep(100);
+        return -1;
     };
-
     cout << "Initialized pigpio" << endl;
-    int spiHandle;
-    while (spiHandle = spiOpen(spiChannel, spiSpeed, 0) < 0)
-    {
-        cout << "Error initializing pigpio SPI" << endl;
-        usleep(100);
-    };
 
+    // open SPI channel
+    int spiHandle = spiOpen(spiChannel, spiSpeed, 0);
+    if (spiHandle < 0)
+    {
+        cerr << "Error initializing pigpio SPI" << endl;
+        usleep(100);
+        return -1;
+    };
     cout << "Initialized SPI" << endl;
 
-    spiData[0] = 0b11010000;
-    // spiData[1] = 0;
-    // spiData[2] = 0;
+    uint16_t iter = 0;
 
-    // cout << static_cast<int>(spiData[0]) << endl;
-    cout << static_cast<int>(spiData[0]) << endl;
-
-    spiReturnVal = spiWrite(spiHandle, spiData, data_len);
-    cout << spiReturnVal << endl;
-
-    cout << static_cast<int>(spiData[0]) << endl;
-
-    if (spiReturnVal != data_len)
-        cout << "SPI transfer error" << endl;
-    else
+    while (true)
     {
-        cout << "SPI transfer successful" << endl;
-        cout << static_cast<int>(spiData[0]) << endl;
+        cout << "Running loop num: " << iter << endl;
+
+        // Set data values to send
+        cout << "Printing Tx data: " << endl;
+        for (int i = 0; i < data_len; i++)
+        {
+            // random data between 0 and 256
+            spiTxData[i] = static_cast<char>(rand() % 256);
+            cout << static_cast<int>(spiTxData[i]) << "\t";
+        }
+        cout << endl;
+
+        int spiReturnVal = spiXfer(spiHandle, spiTxData, spiRxData, data_len);
+        cout << "spiReturnVal = " << spiReturnVal << endl;
+
+        cout << "Printing Rx data: " << endl;
+        for (int i = 0; i < data_len; i++)
+        {
+            cout << static_cast<int>(spiRxData[i]) << "\t";
+        }
+        cout << endl;
+
+        if (spiReturnVal != data_len)
+        {
+            cerr << "SPI transfer error" << endl;
+        }
+        else
+        {
+            cout << "SPI transfer successful" << endl;
+            // cout << static_cast<int>(spiTxData[0]) << endl;
+        }
+
+        cout << "\n----------------" << endl;
+        iter++;
+        usleep(5000 * 1000);
     }
+
+    spiClose(spiHandle);
 
     return 0;
 }
