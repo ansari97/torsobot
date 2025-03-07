6f8c52407ded5d2ecb751bd4cf63dd534ec5172f
Ahmed Ansari PI <ahmedalam97@gmail.com>
2025-03-07 09:00:28 -0600

    SPI demo on pigpio

--- Code/Code/raspberry_pi/pigpio_demo.cpp
+++ Code/Code/raspberry_pi/pigpio_demo.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
-// #include <pigpio.h>
-// #include <unistd.h>
-#include <wiringPi.h>
-#include <wiringPiSPI.h>
+#include <pigpio.h>
+#include <unistd.h>
+// #include <wiringPi.h>
+// #include <wiringPiSPI.h>
 
 using namespace std;
 
 const uint spiChannel = 1;
 const uint spiSpeed = 500 * 1000; // hertz
 
-unsigned char spiData[1];
+const int data_len = 1;
+char spiData[data_len];
 
 int spiReturnVal;
 
 int main()
 {
-    while (wiringPiSetupPinType(WPI_PIN_BCM) != 0)
+    while (gpioInitialise() < 0)
     {
-        cout << "Error initializing wiringPi" << endl;
-        delay(100);
+        cout << "Error initializing pigpio" << endl;
+        usleep(100);
     };
 
-    cout << "Initialized wiringPi" << endl;
-
-    while (wiringPiSPISetup(spiChannel, spiSpeed) < 0)
+    cout << "Initialized pigpio" << endl;
+    int spiHandle;
+    while (spiHandle = spiOpen(spiChannel, spiSpeed, 0) < 0)
     {
-        cout << "Error initializing wiringPi" << endl;
-        delay(100);
+        cout << "Error initializing pigpio SPI" << endl;
+        usleep(100);
     };
 
     cout << "Initialized SPI" << endl;
@@ -38,17 +39,17 @@ int main()
     // cout << static_cast<int>(spiData[0]) << endl;
     cout << static_cast<int>(spiData[0]) << endl;
 
-    spiReturnVal = wiringPiSPIDataRW(spiChannel, spiData, 1);
+    spiReturnVal = spiWrite(spiHandle, spiData, data_len);
     cout << spiReturnVal << endl;
 
     cout << static_cast<int>(spiData[0]) << endl;
 
-    if (spiReturnVal <= 0)
+    if (spiReturnVal != data_len)
         cout << "SPI transfer error" << endl;
     else
     {
         cout << "SPI transfer successful" << endl;
-        cout << spiData[0] << endl;
+        cout << static_cast<int>(spiData[0]) << endl;
     }
 
     return 0;
