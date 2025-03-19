dcb3cf0ec7bdc0c733b0cb7edb75a861aaa1ca64
Ahmed Ansari PI <ahmedalam97@gmail.com>
2025-03-19 10:33:31 -0500

    added example ROS pkg

--- Code/Code/ros2_ws/build/turtlesim/turtlesim__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aansari/torsobot/Code/Code/ros2_ws/build/turtlesim/turtlesim__py
-# Build directory: /home/aansari/torsobot/Code/Code/ros2_ws/build/turtlesim/turtlesim__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
