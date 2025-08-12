2fa0cd72f1ad251bf84b6092709723a11f83b429
Ahmed Ansari PI <ahmedalam97@gmail.com>
2025-08-12 16:37:51 -0500

    modified script

--- Code/Code/ros2_ws/src/torsobot/src/graphing.py
+++ Code/Code/ros2_ws/src/torsobot/src/graphing.py
@@ -17,6 +17,7 @@ def cycles2rad(cycles):
     return cycles*2*np.pi
 
 
+# directory path for the data_logs
 dir_path = "/home/pi/torsobot/Code/Code/ros2_ws/data_logs"
 
 # Get user input for filename
@@ -45,70 +46,69 @@ with open(yaml_file_path, 'r') as yaml_file:
     metadata = yaml.safe_load(yaml_file)
 
 desired_torso_pitch = metadata["/**"]["ros__parameters"]["desired_torso_pitch"]
-mot_max_torque = metadata["/**"]["ros__parameters"]["motor_max_torque"]
+wheel_max_torque = metadata["/**"]["ros__parameters"]["wheel_max_torque"]
 control_max_integral = metadata["/**"]["ros__parameters"]["control_max_integral"]
 kp = metadata["/**"]["ros__parameters"]["kp"]
 ki = metadata["/**"]["ros__parameters"]["ki"]
 kd = metadata["/**"]["ros__parameters"]["kd"]
 
-# print(desired_torso_pitch)
-
-# # convert to radians
-# desired_torso_pitch = rad2deg(desired_torso_pitch)
-# print(desired_torso_pitch)
 
 # plot text
 plot_text = "desired_torso_pitch:" + str(desired_torso_pitch) + "\nkp: " + str(kp) + ", ki: " + str(ki) + ", kd: " + \
-    str(kd) + "\nmax_motor_torque: " + str(mot_max_torque) + \
+    str(kd) + "\wheel_max_torque: " + str(wheel_max_torque) + \
     "\ncontrol_max_integral: " + str(control_max_integral)
 
 # read csv
 df = pd.read_csv(csv_file_path)
 
+# --------------------------- Data----------------------------
 # get data from the csv
-time_value = df["timestamp"]
-imu_pitch_data = df["IMU_pitch"]
-motor_torque = df["motor_trq"]
-motor_cmd_torque = df["motor_cmd_trq"]
-motor_pos = df["motor_pos"]
-motor_vel = df["motor_vel"]
-
-
-# for the plotting
-max_trq_lim = 1.1 * max(max(motor_torque), max(motor_cmd_torque))
-min_trq_lim = 1.1 * min(min(motor_torque), min(motor_cmd_torque))
-
+# "timestamp,torso_pitch,torso_pitch_rate,wheel_pos,wheel_vel,wheel_torque,wheel_cmd_torque,mot_drv_mode";
+timestamp = df["timestamp"]
+torso_pitch = df["torso_pitch"]
+torso_pitch_rate = df["torso_pitch_rate"]
+wheel_pos = df["wheel_pos"]
+wheel_vel = df["wheel_vel"]
+wheel_torque = df["wheel_torque"]
+wheel_cmd_torque = df["wheel_cmd_torque"]
+mot_drv_mode = df["mot_drv_mode"]
 
 # change data
-time_value = time_value/(10**6)  # ns to ms
+timestamp = timestamp/(10**6)  # ns to ms
 
-zero_time = time_value[0]
-time_value = time_value - zero_time
+zero_time = timestamp[0]
+timestamp = timestamp - zero_time
 
-start_time = time_value > 50000
+start_time = int(input("Start time in ms: "))
+# get values where time is more than start_time
+start_time = timestamp > start_time
 start_idx = df[start_time].index[0]
 
-# print(time_value)
-print(start_idx)
-time_value = time_value[start_idx:-1]
-imu_pitch_data = rad2deg(imu_pitch_data[start_idx:-1])
-motor_pos = cycles2rad(motor_pos[start_idx:-1])
-motor_vel = cycles2rad(motor_vel[start_idx:-1])
-motor_cmd_torque = motor_cmd_torque[start_idx:-1]
-motor_torque = motor_torque[start_idx:-1]
+# get values from start_idx to end
+timestamp = timestamp[start_idx:-1]
+torso_pitch = rad2deg(torso_pitch[start_idx:-1])
+wheel_pos = wheel_pos[start_idx:-1]
+wheel_vel = wheel_vel[start_idx:-1]
+wheel_cmd_torque = wheel_cmd_torque[start_idx:-1]
+wheel_torque = wheel_torque[start_idx:-1]
 
-
-desired_torso_pitch = np.ones(len(time_value)) * \
+desired_torso_pitch = np.ones(len(timestamp)) * \
     desired_torso_pitch  # df["desired_pitch_data"]
 # print(desired_torso_pitch)
+
+# --------------------------- Plotting----------------------------
+
+# plotting limits
+max_trq_lim = 1.1 * max(max(wheel_torque), max(wheel_cmd_torque))
+min_trq_lim = 1.1 * min(min(wheel_torque), min(wheel_cmd_torque))
 
 fig, axs = plt.subplots(4, 1, figsize=(15, 8))
 fig.suptitle(file_date + "---" + file_time)
 
 # Subplot 1
-axs[0].plot(time_value, desired_torso_pitch,
-            color="red", label="desired pitch")
-axs[0].plot(time_value, imu_pitch_data, label="imu_pitch")
+axs[0].plot(timestamp, desired_torso_pitch,
+            color="red", label="desired torso pitch")
+axs[0].plot(timestamp, torso_pitch, label="actual torso pitch")
 
 # draw horizontal line for pitch
 # plt.axhline(desired_torso_pitch)
@@ -116,8 +116,8 @@ axs[0].plot(time_value, imu_pitch_data, label="imu_pitch")
 axs[0].minorticks_on()
 axs[0].grid(which="both")
 
-axs[0].set_ylabel("IMU pitch (deg)")
-axs[0].set_ylim([-10, 380])
+axs[0].set_ylabel("torso pitch (deg)")
+# axs[0].set_ylim([-10, 380])
 # axs[0].set_xlim(xmin=0)
 axs[0].legend()
 axs[0].set_title(plot_text, fontsize=10)
@@ -125,31 +125,33 @@ axs[0].set_title(plot_text, fontsize=10)
 #             bbox=dict(facecolor='yellow', alpha=0.75))
 
 # Subplot 2
-axs[1].plot(time_value, motor_torque, label="motor_torque")
-axs[1].plot(time_value, motor_cmd_torque,
-            color="red", label="motor_cmd_torque")
+axs[1].plot(timestamp, wheel_torque, label="wheel_torque")
+axs[1].plot(timestamp, wheel_cmd_torque,
+            color="red", label="wheel_cmd_torque")
 axs[1].minorticks_on()
 axs[1].grid(which="both")
-axs[1].set_xlabel("time (ms)")
+# axs[1].set_xlabel("time (ms)")
 axs[1].set_ylabel("torque (N.m)")
 axs[1].set_ylim([min_trq_lim, max_trq_lim])
 axs[1].legend()
 
 # Subplot 3
-axs[2].plot(time_value, motor_pos, label="motor_pos")
+axs[2].plot(timestamp, wheel_pos, label="wheel_pos")
 axs[2].minorticks_on()
 axs[2].grid(which="both")
-axs[2].set_xlabel("time (ms)")
-axs[2].set_ylabel("motor pos (cycles)")
+axs[2].axhline(np.pi/10, color="red")
+axs[2].axhline(-np.pi/10, color="red")
+# axs[2].set_xlabel("time (ms)")
+axs[2].set_ylabel("wheel pos (rad)")
 # axs[2].set_ylim([min_trq_lim, max_trq_lim])
 axs[2].legend()
 
 # Subplot 4
-axs[3].plot(time_value, motor_vel, label="motor_vel")
+axs[3].plot(timestamp, wheel_vel, label="wheel_vel")
 axs[3].minorticks_on()
 axs[3].grid(which="both")
 axs[3].set_xlabel("time (ms)")
-axs[3].set_ylabel("motor velocity (cycles/s)")
+axs[3].set_ylabel("wheel velocity (rad/s)")
 # axs[3].set_ylim([min_trq_lim, max_trq_lim])
 axs[3].legend()
 
